a13a71ddd77d70230733d4a2e2a3bee44c5f6a51
LietuvisKalba <mantasling@gmail.com>
2018-12-27 00:07:33 +0000

    Balls can now fall

---
+++ Scripts/Chest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour {
+
+    public Animator anim;
+
+    private bool isOpen;
+
+    void Start()
+    {
+        isOpen = false;
+    }
+
+	void FixedUpdate () {
+		
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(toMouse, out hit))
+            {
+                OpenCloseChest();
+                anim.SetTrigger("isLidOpen");
+                Debug.Log("the chest is open?: " + isOpen);
+            }
+        }
+	}
+
+    void OpenCloseChest()
+    {
+        if (isOpen == false)
+        {
+            isOpen = true;
+        }
+        else
+        {
+            isOpen = false;
+        }
+    }
+}
---
+++ Scripts/Items.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private float fallSpeed = 20f;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        rb.velocity = new Vector3(0,-1,0) * fallSpeed;
+	}
+}
